d846e7a3bff1504b56157838711b43d9997a1070
Krishna227-Cool <77683735+Krishna227-Cool@users.noreply.github.com>
2021-04-26 21:32:14 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+var imge = ["https://www.how-to-draw-funny-cartoons.com/image-files/cartoon-dad-005.jpg", "https://www.how-to-draw-funny-cartoons.com/image-files/cartoon-mom-005.jpg"]
+var i = 0;
+
+function next() {
+    document.getElementById("imagepog").src=imge[i];
+  i++;
+}
